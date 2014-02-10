a0e245e2a91e2e1274564c01fb6dc1252d54b4dd
Kees Kluskens <keeskluskens@gmail.com>
2014-02-10 20:51:39 +0100

    Heel simpele rooster weergave gemaakt

--- week.php
+++ week.php
@@ -1,32 +1,52 @@
 <?php
 require 'inc/calfileparser.php';
 
-$cal = new CalFileParser();
-$remote = $cal->parse('http://pinega.fontys.nl/studenten/ical.aspx?instituut=1&klas=m22');
+
 
 // echo '<pre>';
 // print_r($remote);
 
 $huidigWeeknummer = date('W');
 
-foreach ($remote as $event) {
-  $starttijd = strtotime($event['dtstart;tzid=europe/amsterdam']); // Starttijd geconverteerd naar UNIX timestamp
-  $eindtijd  = strtotime($event['dtend;tzid=europe/amsterdam']); // Eindtijd geconverteerd naar UNIX timestamp
-  $starttijdHuman = date('l H:i', $starttijd);
-  $eindtijdHuman  = date('H:i', $eindtijd);
+function getDag($weekNummer, $dagNummer) {
+  $cal = new CalFileParser();
+  $remote = $cal->parse('http://pinega.fontys.nl/studenten/ical.aspx?instituut=1&klas=m22');
 
-  $dagBegonnen = 0;
+  foreach ($remote as $event) {
+    $starttijd = strtotime($event['dtstart;tzid=europe/amsterdam']); // Starttijd geconverteerd naar UNIX timestamp
+    $eindtijd  = strtotime($event['dtend;tzid=europe/amsterdam']); // Eindtijd geconverteerd naar UNIX timestamp
+    $starttijdHuman = date('H:i', $starttijd);
+    $eindtijdHuman  = date('H:i', $eindtijd);
 
-  // Alleen de huidige week weergeven
-  if (date('W', $starttijd) === $huidigWeeknummer) {
-    // if (!$dagBegonnen) {
-    //   echo "<h2>".date('l', $starttijd)."</h2>";
-    //   $dagBegonnen = 1;
-    // }
-    echo "<b>".$starttijdHuman." - ";
-    echo $eindtijdHuman."</b><br/>";
-    echo $event['summary']."<br/>";
-    echo "<small>".$event['description']."</small><br/>";
-    echo "<hr/>";
+    // Alleen de huidige week weergeven
+    if (date('W', $starttijd) === $weekNummer AND date('N', $starttijd) == $dagNummer) {
+      echo "<b>".$starttijdHuman." - ";
+      echo $eindtijdHuman."</b><br/>";
+      echo $event['summary']."<br/>";
+      echo "<small>".$event['description']."</small><br/>";
+      echo "<hr/>";
+    }
   }
 }
+
+// Haal het rooster van maandag van deze week op
+?>
+
+<div class="row">
+  <div class="large-3 columns">
+    <h2>Ma</h2>
+    <?php echo getDag(date('W', time()), 1); ?>
+  </div>
+  <div class="large-3 columns">
+    <h2>Di</h2>
+    <?php echo getDag(date('W', time()), 2); ?>
+  </div>
+  <div class="large-3 columns">
+    <h2>Wo</h2>
+    <?php echo getDag(date('W', time()), 3); ?>
+  </div>
+  <div class="large-3 columns">
+    <h2>Do</h2>
+    <?php echo getDag(date('W', time()), 4); ?>
+  </div>
+</div>
