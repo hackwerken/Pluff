7ad56d865f7c92274e38ce9221727e9bdfaef82b
Kees Kluskens <keeskluskens@gmail.com>
2014-02-11 22:37:47 +0100

    CSS verbeterd & IE browser detectie toegevoegd

--- index.php
+++ index.php
@@ -17,6 +17,17 @@ require 'inc/header.php';
 
 </head>
 <body>
+  <!--[if lt IE 9]>
+    <div class="slechtebrowser">
+      <p>
+        Het lijkt erop dat je Internet Explorer 8 of lager gebruikt. Het internet gaat snel vooruit, maar toch komt je browser nog uit het jaar 2009. We zitten nu in 2014. Dit kan toch niet?
+      </p>
+      <p>
+        Ik adviseer je sterk om een goede internetbrowser zoals <a href="https://www.google.nl/intl/nl/chrome/browser/">Chrome</a> of <a href="https://www.mozilla.org/nl/firefox/">Firefox</a> te downloaden. Bezoek daarna mijn site en check het verschil!
+      </p>
+    </div>
+  <![endif]-->
+
   <div class="row">
     <div class="small-15 columns">
       <div class="header">
@@ -27,19 +38,26 @@ require 'inc/header.php';
   </div>
   <div class="row">
     <div class="small-15 medium-5 columns">
-      <a href="rooster.php?klas=<?php echo $klasOrig ?>&week=<?php echo $weeknr_vorige ?>" class="button alert vorige-week js-vorige">&laquo; Vorige week</a>
+      <a href="index.php?klas=<?php echo $klasOrig ?>&week=<?php echo $weeknr_vorige ?>" class="button alert vorige-week js-vorige">&laquo; Vorige week</a>
     </div>
     <div class="small-15 medium-5 text-center-large columns">
-      <a href="rooster.php?klas=<?php echo $klasOrig ?>&week=<?php echo $weeknr_huidig ?>" class="button huidige-week js-huidige">Huidige week</a>
+      <a href="index.php?klas=<?php echo $klasOrig ?>&week=<?php echo $weeknr_huidig ?>" class="button huidige-week js-huidige">Huidige week</a>
     </div>
     <div class="small-15 medium-5 columns">
-      <a href="rooster.php?klas=<?php echo $klasOrig ?>&week=<?php echo $weeknr_volgende ?>" class="button success volgende-week js-volgende">Volgende week &raquo;</a>
+      <a href="index.php?klas=<?php echo $klasOrig ?>&week=<?php echo $weeknr_volgende ?>" class="button success volgende-week js-volgende">Volgende week &raquo;</a>
     </div>
   </div>
   <div class="row">
     <div class="small-15 columns">
       <?php if (empty($_GET['klas'])) : ?>
-        <h3>Klas</h3>
+        <h3>Rooster</h3>
+        <p>
+          Hieronder kun je je klas van Fontys invoeren. Zit je in meerdere klassen? Scheid deze dan met een puntkomma.
+          Je kunt hierna onderaan de pagina de &lsquo;permalink&rsquo; kopi&euml;ren en hier eventueel een bladwijzer van maken.
+        </p>
+        <p>
+          <strong>Note:</strong> Je kunt hier alleen gebruik van maken als je op de Fontys ICT opleiding zit. We zijn nog bezig met andere opleidingen toevoegen.
+        </p>
         <input type="text" value="" placeholder="Vul een of meerdere klassen in (puntkommmagescheiden)" class="js-klas">
       <?php endif; ?>
       <div class="hetrooster">
@@ -64,7 +82,7 @@ require 'inc/header.php';
     <div class="row">
       <div class="small-15 columns">
         <p>
-          BETA Versie. Gemaakt door <a href="https://webduck.nl">Kees Kluskens</a> &amp; <a href="http://laylo.nl">Jeroen Janssen</a>.
+          <em>Pluff</em> BETA Versie. Gemaakt door <a href="https://webduck.nl">Kees Kluskens</a> &amp; <a href="http://laylo.nl">Jeroen Janssen</a>.
           Denk je dat dit beter kan? Help ons op <a href="https://github.com/SpaceK33z/FHICT-Rooster">Github</a>!
         </p>
       </div>
