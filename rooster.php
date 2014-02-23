06f785d70e829db99479d9dff422f8984c9222aa
Kees Kluskens <keeskluskens@gmail.com>
2014-02-23 23:34:41 +0100

    Eerste opzet van nieuwe layout

--- rooster.php
+++ rooster.php
@@ -6,24 +6,34 @@ require 'inc/header.php';
 <?php if (!empty($klas)) :?>
   <div class="row">
     <div class="medium-3 columns">
-      <h2>Ma</h2>
-      <?php echo getDag($weeknr, 1, $klas); ?>
+      <div class="dag">
+        <h4>Maandag</h4>
+        <?php echo getDag($weeknr, 1, $klas); ?>
+      </div>
     </div>
     <div class="medium-3 columns">
-      <h2>Di</h2>
-      <?php echo getDag($weeknr, 2, $klas); ?>
+      <div class="dag">
+        <h4>Dinsdag</h4>
+        <?php echo getDag($weeknr, 2, $klas); ?>
+      </div>
     </div>
     <div class="medium-3 columns">
-      <h2>Wo</h2>
-      <?php echo getDag($weeknr, 3, $klas); ?>
+      <div class="dag">
+        <h4>Woensdag</h4>
+        <?php echo getDag($weeknr, 3, $klas); ?>
+      </div>
     </div>
     <div class="medium-3 columns">
-      <h2>Do</h2>
-      <?php echo getDag($weeknr, 4, $klas); ?>
+      <div class="dag">
+        <h4>Donderdag</h4>
+        <?php echo getDag($weeknr, 4, $klas); ?>
+      </div>
     </div>
     <div class="medium-3 columns">
-      <h2>Vr</h2>
-      <?php echo getDag($weeknr, 5, $klas); ?>
+      <div class="dag">
+        <h4>Vrijdag</h4>
+        <?php echo getDag($weeknr, 5, $klas); ?>
+      </div>
     </div>
   </div>
 <?php endif; ?>
