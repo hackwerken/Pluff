79ca1f9fd9a2b361ddeb1394af6f8c1d078a8acf
Kees Kluskens <keeskluskens@gmail.com>
2014-03-06 16:37:04 +0100

    Meerdere uren tegelijkertijd zijn nu mogelijk

--- inc/json.php
+++ inc/json.php
@@ -3,15 +3,10 @@
  * Dit bestand haalt het daadwerkelijk rooster via JSON op en verwerkt het.
  */
 
-// Accepteert array met klassen
-function getFile($klassen) {
-  global $klas_whitelist, $cache_dir;
-
-
-}
-
 function getUur($weekNummer, $dagNummer, $uurNummer, $klassen) {
   global $db;
+
+  $uur = array();
 
   foreach ($klassen as $klas) {
     // Huidige jaar pakken met het ingevoerde week nummer.
@@ -20,14 +15,24 @@ function getUur($weekNummer, $dagNummer, $uurNummer, $klassen) {
     $datum = date('Y-m-d', $begintijd);
 
     $query = $db->query("SELECT * FROM rooster WHERE klas LIKE '%".$klas."%' AND tijdstip_begin LIKE '%".$datum."%' AND uurnr_begin = '".$uurNummer."'");
-    $uur = $query->fetch(PDO::FETCH_ASSOC);
+    $fetch = $query->fetch(PDO::FETCH_ASSOC);
+
+    // Als er een uur gevonden is, deze in de array zetten (zodat er meerdere lesuren in één uur kunnen zitten)
+    if ($fetch)
+      $uur[] = $fetch;
   }
 
   return $uur;
 }
 
-function huidigeDag($dagNummer) {
-  if (date('N') == $dagNummer) {
+function huidigeDag($weekNummer, $dagNummer) {
+  // Converteer het ingevoerde weeknummer + dagnummer van de week naar een UNIX timestamp
+  $datum = strtotime(date('Y').'W'.$weekNummer.' + '.($dagNummer - 1).' day');
+  // Pak dan het dagnummer van het jaar (0 - 365)
+  $dagnrvanhetjaar = date('z', $datum);
+
+  // En controleer of het de huidige dag is. Zoja, return true
+  if (date('z') == $dagnrvanhetjaar) {
     return true;
   }
   return false;
--- rooster.php
+++ rooster.php
@@ -8,23 +8,28 @@ require 'inc/header.php';
   <div class="row">
     <?php foreach($cDagen as $dagnr => $dagNaam) : ?>
       <div class="medium-3 columns">
-        <div class="dag <?php echo (huidigeDag($dagnr)) ? 'huidige-dag' : '' ?>">
+        <div class="dag <?php echo (huidigeDag($weeknr, $dagnr)) ? 'huidige-dag' : '' ?>">
           <h4><?php echo $dagNaam ?></h4>
           <?php foreach (array_slice($cTijden, 0, -1, true) as $uurnr => $uurtijd) : ?>
             <div class="uur">
+              <div class="uur-nummer"><?php echo $uurnr ?></div>
               <?php
-              $uur = getUur($weeknr, $dagnr, $uurnr, $klas);
-              if ($uur) {
-                echo '<b>'.date('H:i', strtotime($uur['tijdstip_begin'])).' - '.date('H:i', strtotime($uur['tijdstip_eind'])).'</b><br>';
-                echo $uur['vak'].' - '.$uur['docent'].'<br>';
-                echo '<small>'.$uur['lokaal'].' - '.$uur['klas'].'</small><br>';
+              $uurArray = getUur($weeknr, $dagnr, $uurnr, $klas);
+              if ($uurArray) {
+                foreach ($uurArray as $uur) {
+                  if ($uur) {
+                    echo '<b>'.date('H:i', strtotime($uur['tijdstip_begin'])).' - '.date('H:i', strtotime($uur['tijdstip_eind'])).'</b><br>';
+                    echo $uur['vak'].' - '.$uur['docent'].'<br>';
+                    echo '<small>'.$uur['lokaal'].' - '.$uur['klas'].'</small><br>';
+                  }
+                }
               }
               else {
                 echo 'leeg<br>';
               }
               ?>
             </div>
-            <hr>
+            <hr class="<?php echo ($uurnr === 14) ? 'hide' : '' ?>">
           <?php endforeach ?>
         </div>
       </div>
