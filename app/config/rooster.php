01d75d4942d403ca3a9b9cbabb0b8f379e697ce7
Kees Kluskens <kees@webduck.nl>
2014-04-21 16:49:43 +0200

    Documentatie van config verbeteren

--- app/config/rooster.php
+++ app/config/rooster.php
@@ -1,8 +1,34 @@
 <?php
 
 return [
+  /*
+  |--------------------------------------------------------------------------
+  | Het versienummmer
+  |--------------------------------------------------------------------------
+  |
+  | Verschijnt onder in de footer.
+  |
+  */
   'versie' => '1.0',
+  /*
+  |--------------------------------------------------------------------------
+  | Fetch URL
+  |--------------------------------------------------------------------------
+  |
+  | Deze URL wordt gebruikt om de roosters uit op te halen. De response moet
+  | in JSON zijn. Achter deze URL komt een klasnaam te staan.
+  |
+  */
   'fetchUrl' => 'http://iplanner.fontys.nl/RoosterHandler.ashx?soort=JSON&instituut=1&klas=',
+  /*
+  |--------------------------------------------------------------------------
+  | Lesuur tijden
+  |--------------------------------------------------------------------------
+  |
+  | Dit zijn de uren waarop een lesuur kan _beginnen_. Een lesuur hoeft niet
+  | per se op ondertaande tijdstippen te _eindigen_.
+  |
+  */
   'tijden' => [
     1 => '08:45',
     2 => '09:35',
@@ -20,12 +46,37 @@ return [
     14 => '20:50',
     15 => '21:40'
   ],
+  /*
+  |--------------------------------------------------------------------------
+  | Klas filter
+  |--------------------------------------------------------------------------
+  |
+  | Dit zijn de klassen / docenten / lokalen die uit de cheatsheet worden
+  | gefilterd.
+  |
+  */
+
   'klas_filter' => [
     '-',
     '?',
     '',
     'xxx'
   ],
+  /*
+  |--------------------------------------------------------------------------
+  | Vakanties
+  |--------------------------------------------------------------------------
+  |
+  | Dit zijn, logischerwijs alle vakanties die er nog komen. Zorg dat ze
+  | in dit exacte formaat staan:
+  | 'vakanties' => [
+  |   'Vakantienaam' => [
+  |     'start' => 'yyyy-mm-dd',
+  |     'eind' => 'yyyy-mm-dd'
+  |   ]
+  | ]
+  |
+  */
   'vakanties' => [
     'Meivakantie' => [
       'start' => '2014-04-26',
