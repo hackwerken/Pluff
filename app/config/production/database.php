fe4b070fb02f20516868875556ec57034b9e70dc
Kees Kluskens <kees@webduck.nl>
2014-05-09 11:06:46 +0200

    Productie database naar Postgresql

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -2,15 +2,15 @@
 
 return array(
   'connections' => array(
-    'mysql' => array(
-      'driver'    => 'mysql',
-      'host'      => getenv('DB_HOST'),
-      'database'  => getenv('DB_NAME'),
-      'username'  => getenv('DB_USER'),
-      'password'  => getenv('DB_PASSWORD'),
-      'charset'   => 'utf8',
-      'collation' => 'utf8_unicode_ci',
-      'prefix'    => '',
+    'pgsql' => array(
+      'driver'   => 'pgsql',
+      'host'     => getenv('DB_HOST'),
+      'database' => getenv('DB_NAME'),
+      'username' => getenv('DB_USER'),
+      'password' => getenv('DB_PASSWORD'),
+      'charset'  => 'utf8',
+      'prefix'   => '',
+      'schema'   => 'public',
     )
   )
 );
