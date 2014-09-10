9bd73128df1ed27b90a22d57e24cf1adf5ba1160
Kees Kluskens <kees@webduck.nl>
2014-09-10 13:23:11 +0200

    Stupide animatie toch weer weg

--- app/views/graph-klas.blade.php
+++ app/views/graph-klas.blade.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <title>Pluff | @lang('site.page_title')</title>
 
-  <link rel="stylesheet" href="/css/graph.css?94df6e">
+  <link rel="stylesheet" href="/css/graph.css?0d2660">
 
   <link rel="apple-touch-icon" sizes="57x57" href="/img/apple-touch-icon-57x57.png">
   <link rel="apple-touch-icon" sizes="114x114" href="/img/apple-touch-icon-114x114.png">
