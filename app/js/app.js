a5b60ced00eb305422078fda1e695920eeff7698
Kees Kluskens <kees@webduck.nl>
2014-09-15 22:00:11 +0200

    Eerste experimenten met Angular

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,14 @@
-'use strict';
+$.post('https://apps.fhict.nl/api/v1/people/me?callback=?',function(data,textStatus,jqXHR){
+  console.log(data);
+});
+// OUTPUT:
+// XMLHttpRequest cannot load https://apps.fhict.nl/api/v1/people/me?callback=?.
+// No 'Access-Control-Allow-Origin' header is present on the requested resource.
+// Origin 'http://localhost:9000' is therefore not allowed access.
 
-/* App Module */
+
+$.getJSON('https://apps.fhict.nl/api/v1/people/me?callback=?',function(data,textStatus,jqXHR){
+  console.log(data);
+});
+// OUTPUT:
+// Status Code:405 Method Not Allowed
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,15 +2,56 @@
 
 /* Controllers */
 
-var pluffApp = angular.module('pluffApp', []);
+var pluffApp = angular.module('pluffApp', [])
+  .controller('StudentCtrl', ['$scope', '$http', StudentCtrl])
+  .controller('RoosterCtrl', ['$scope', RoosterCtrl]);
 
-pluffApp.controller('PhoneListCtrl', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
+var APIurl = 'https://apps.fhict.nl/api/v1';
+var APIcallback = '?callback=JSON_CALLBACK';
+
+function StudentCtrl($scope, $http) {
+  // $http({
+  //   method: 'POST',
+  //   url: APIurl + '/people/me' + APIcallback,
+  //   success:
+  // });
+  $http.jsonp(APIurl + '/people/important' + APIcallback)
+  .success(function(data, status) {
+    console.log(data);
+  })
+  .error(function(data, status) {
+    // Redirect naar FHICT inlogpagina
+    window.location = 'https://portal.fhict.nl/CookieAuth.dll?GetLogon?reason=0&formdir=6';
+  });
+}
+
+function RoosterCtrl($scope) {
+  $scope.days = [
+    {number: 1, spelled: 'Maandag'},
+    {number: 2, spelled: 'Dinsdag'},
+    {number: 3, spelled: 'Woensdag'},
+    {number: 4, spelled: 'Donderdag'},
+    {number: 5, spelled: 'Vrijdag'}
   ];
-});
+
+  $scope.hours = [
+    {number: 1, start: '08:45'},
+    {number: 2, start: '09:35'},
+    {number: 3, start: '10:45'},
+    {number: 4, start: '11:35'},
+    {number: 5, start: '12:25'},
+    {number: 6, start: '13:15'},
+    {number: 7, start: '14:05'},
+    {number: 8, start: '15:15'},
+    {number: 9, start: '16:05'},
+    {number: 10, start: '16:55'},
+    {number: 11, start: '18:00'},
+    {number: 12, start: '18:50'},
+    {number: 13, start: '20:00'},
+    {number: 14, start: '21:40'}
+  ];
+
+  $scope.test = function(day) {
+    alert('Dit is dag ' + day);
+  }
+}
