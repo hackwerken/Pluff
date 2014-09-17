b382bd8f6ee63738ecc76009744867423d6f41f3
Kees Kluskens <kees@webduck.nl>
2014-09-17 13:05:35 +0200

    Proof of concept

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,28 +4,25 @@
 
 var pluffApp = angular.module('pluffApp', [])
   .controller('StudentCtrl', ['$scope', '$http', StudentCtrl])
-  .controller('RoosterCtrl', ['$scope', RoosterCtrl]);
+  .controller('RoosterCtrl', ['$scope', '$http', RoosterCtrl]);
 
 var APIurl = 'https://apps.fhict.nl/api/v1';
-var APIcallback = '?callback=JSON_CALLBACK';
+var APIcallback = '&callback=JSON_CALLBACK';
 
 function StudentCtrl($scope, $http) {
-  // $http({
-  //   method: 'POST',
-  //   url: APIurl + '/people/me' + APIcallback,
-  //   success:
+  // $http.jsonp(APIurl + '/people/important' + APIcallback)
+  // .success(function(data, status) {
+  //   console.log(data);
+  // })
+  // .error(function(data, status) {
+  //   // Redirect naar FHICT inlogpagina
+  //   window.location = 'https://portal.fhict.nl/CookieAuth.dll?GetLogon?reason=0&formdir=6';
   // });
-  $http.jsonp(APIurl + '/people/important' + APIcallback)
-  .success(function(data, status) {
-    console.log(data);
-  })
-  .error(function(data, status) {
-    // Redirect naar FHICT inlogpagina
-    window.location = 'https://portal.fhict.nl/CookieAuth.dll?GetLogon?reason=0&formdir=6';
-  });
 }
 
-function RoosterCtrl($scope) {
+function RoosterCtrl($scope, $http) {
+  $scope.roosterData = false;
+
   $scope.days = [
     {number: 1, spelled: 'Maandag'},
     {number: 2, spelled: 'Dinsdag'},
@@ -34,6 +31,7 @@ function RoosterCtrl($scope) {
     {number: 5, spelled: 'Vrijdag'}
   ];
 
+  // TODO: start times probably aren't necessary (?)
   $scope.hours = [
     {number: 1, start: '08:45'},
     {number: 2, start: '09:35'},
@@ -51,7 +49,47 @@ function RoosterCtrl($scope) {
     {number: 14, start: '21:40'}
   ];
 
-  $scope.test = function(day) {
-    alert('Dit is dag ' + day);
-  }
+  // Get the personal schedule from the API
+  $http.jsonp(APIurl + '/Schedule/me?daysAhead=90' + APIcallback)
+    .success(function(data, status) {
+      $scope.roosterData = data.data; // sorry for the awful names it's late okay?
+    })
+    .error(function(data, status) {
+      // Redirect to FHICT loginpage if there's an error, because the user probably isn't logged in
+      // TODO: Check if it's a 'normal' error or auth error
+      // TODO: Redirect back to Pluff
+      window.location = 'https://portal.fhict.nl/CookieAuth.dll?GetLogon?reason=0&formdir=6';
+    });
+
+  $scope.roosterDate = moment();
+
+  $scope.weekNumber = 38;
+
+  $scope.getHour = function(dayNumber, hourNumber) {
+    // console.log('dagnr: ' + dayNumber + ' en uurnr:' + hourNumber);
+    var hourCallback;
+
+    if ($scope.RoosterData != false) {
+      $scope.roosterData.forEach(function(les) {
+        // Get the input date from the weeknumber of the year and daynumber of week (1-5) (ex. 2014-52-5)
+        var currentDay = moment('2014-'+ $scope.weekNumber + '-' + dayNumber, 'YYYY-ww-d');
+
+        // Check if the lesson is on the current day
+        if (currentDay.isSame(les.start, 'day')) {
+          // Get the exponent of the hourNumber (^2 - 1)
+          var hourBin = Math.pow(2, hourNumber-1);
+
+          if (les.hoursMask & hourBin) {
+            hourCallback = hourNumber + ' er is les';
+          }
+        }
+      });
+
+      return hourCallback;
+    }
+
+    // hourNumber is 1 to 15
+
+
+  };
 }
