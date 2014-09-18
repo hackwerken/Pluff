953928a79bb742c3e46e676f492efcb7e8e0434e
Kees Kluskens <kees@webduck.nl>
2014-09-18 15:31:19 +0200

    Gigantische performance boost mbt het uur ophalen

--- app/js/services.js
+++ app/js/services.js
@@ -13,21 +13,19 @@ angular.module('pluffApp.services', [])
       // Allow multiple lessons in one hour
       var hourCallback = [];
 
+      // Get the input date from the weeknumber of the year and daynumber of week (1-5) (ex. 2014-52-5)
+      var currentDay = moment('2014-' + $scope.weekNumber().use + '-' + dayNumber, 'YYYY-w-d');
+
+      // Get the exponent of the hourNumber (^2 - 1)
+      var hourBin = Math.pow(2, hourNumber - 1);
+
       // Wait until the JSON is fully loaded
       if ($scope.tableData != false) {
         // TODO: Optimize for performance (!!)
         $scope.tableData.forEach(function(les) {
-          // Get the input date from the weeknumber of the year and daynumber of week (1-5) (ex. 2014-52-5)
-          var currentDay = moment('2014-' + $scope.weekNumber().use + '-' + dayNumber, 'YYYY-w-d');
-
           // Check if the lesson is on the current day
-          if (currentDay.isSame(les.start, 'day')) {
-            // Get the exponent of the hourNumber (^2 - 1)
-            var hourBin = Math.pow(2, hourNumber - 1);
-
-            if (les.hoursMask & hourBin) {
-              hourCallback.push(les);
-            }
+          if (currentDay.isSame(les.start, 'day') && les.hoursMask & hourBin) {
+            hourCallback.push(les);
           }
         });
       }
