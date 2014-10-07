e358d7f5b792a4c5debdc9afc36b7e0cc1cb8125
Kees Kluskens <kees@webduck.nl>
2014-10-07 10:39:07 +0200

    Nieuwe login URL

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 var APIconfig = {
   rawUrl: 'https://apps.fhict.nl/api/v1',
   callback: '&callback=JSON_CALLBACK',
-  loginUrl: 'https://portal.fhict.nl/CookieAuth.dll?GetLogon?reason=0&formdir=6',
+  loginUrl: 'https://apps.fhict.nl/link/pluff',
   url: function(url) {
     // Enclose the given _relative_ url with the absolute url + callback.
     // TODO: Replace ampersand with questionmark if needed (maybe not necessary?)
