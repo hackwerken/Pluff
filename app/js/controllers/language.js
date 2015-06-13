bd873c082306e900c86d7156b9c1e472fc845f55
Kees Kluskens <kees@webduck.nl>
2015-06-13 18:30:29 +0200

    Fix language switching on Safari. Fixes #176

--- app/js/controllers/language.js
+++ app/js/controllers/language.js
@@ -2,8 +2,12 @@ appCtrls.controller('LanguageCtrl', function($scope, $translate, $window) {
   $scope.switch = function($lang) {
     // Switch to the given language
     $translate.use($lang);
+
     // Full page reload to apply all languages
     // This is necessary because of the one-time bindings used for performance reasons
-    $window.location.reload();
+    // setTimeout() is necessary because otherwise Safari doesn't set the cookie
+    setTimeout(function () {
+      $window.location.reload();
+    });
   };
 });
