ba97395ac0c519a954124d0449611ba9e3f0f579
Kees Kluskens <kees@webduck.nl>
2015-08-23 23:32:33 +0200

    Show a dialog if the user is not authenticated yet. #163

--- app/js/services/api.js
+++ app/js/services/api.js
@@ -1,26 +1,40 @@
-appServices.factory('apiService', function($http, $auth) {
-  function authorize() {
+appServices.factory('apiService', function($http, $auth, $q, ngDialog) {
+  function validateAuth() {
     if (!$auth.isAuthenticated()) {
+      // Show dialog about why the user must authenticate.
+      var dialog = ngDialog.open({
+        template: 'partials/dialog-authenticate.html',
+        showClose: false,
+        closeByEscape: false,
+        closeByDocument: false
+      });
+
       return $auth.authenticate('fhict').then(function () {
-        return true;
-      }, function () {
-        console.warn('FHICT authentication went wrong.');
+        // User is successfully authenticated now, so close the dialog.
+        dialog.close();
+      }, function (data) {
+        // TODO: Create a nice dialog explaining that something weird went wrong.
+        // Don't know yet when this would occur.
+        console.error('FHICT authentication went wrong.', data);
       });
     }
 
-    return true;
+    // Create a fake promise if already authenticated.
+    // Hm, there must be a better solution...
+    var deferred = $q.defer();
+    deferred.resolve();
+    return deferred.promise;
   }
 
   function get(url, params) {
-    if (authorize()) {
+    // Authenticate before trying to load the url.
+    return validateAuth().then(function () {
       return $http({
         url: 'https://tas.fhict.nl/api/v1' + url,
         method: 'GET',
         params: params
       });
-    }
-
-    console.warn('Shit went wrong.');
+    });
   }
 
   return {
