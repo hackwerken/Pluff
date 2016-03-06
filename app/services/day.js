60a69e3f0be0d6c501176fcf7a670b91a719aaf9
Kees Kluskens <kees@webduck.nl>
2016-03-06 23:53:26 +0100

    Fix incorrect start of breaks

--- app/services/day.js
+++ app/services/day.js
@@ -13,7 +13,7 @@ export default function (weekService) {
   updateTime();
 
   // List of the breaks and the duration. The first break is after the second hour and is 20 minutes.
-  data.hourBreaks = [0, 0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 30, 0, 30, 0];
+  data.hourBreaks = [0, 15, 0, 0, 0, 0, 15, 0, 0, 0, 30, 0, 30, 0];
   // List of hours
   data.hourNumbers = [
     { number: 1, duration: 45 },
